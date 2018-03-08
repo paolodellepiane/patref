cdd797765c5027de48b3a022cb29da6d33f2d973
pd <paolo.dellepiane@microarea.it>
2018-03-08 17:29:28 +0100

    .

--- Program.fs
+++ Program.fs
@@ -1,8 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.Threading
+let addPoint (ctx:HttpContext) = async {
+  return Some ctx
+}
 
-open System
+let app =
+  choose
+    [ GET >=> choose
+        [ path "/space" >=> OK "Good bye GET"
+          path "/lines/%d" >=> OK "Good bye GET" ]
+      POST >=> choose
+        [ path "/point" >=> addPoint ] 
+      DELETE >=> choose
+        [ path "/point/%d" >=> OK "" 
+          path "/space" >=> OK "Hello GET" ]
+    ]
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let cts = new CancellationTokenSource()
+    let conf = { defaultConfig with bindings = [ HttpBinding.createSimple HTTP "0.0.0.0" 8083 ];
+                                cancellationToken = cts.Token }
+    let _, server = startWebServerAsync conf app
+    Async.Start(server, cts.Token)
+    printfn "Make requests now"
+    Console.ReadKey true |> ignore
+    0
